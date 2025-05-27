2596dae321cb3f48ccad3fc7c82a2ad4172b72f1
honopio <honorine.piolat@gmail.com>
2025-05-27 19:37:33 +0200

    feat: add a basic header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,41 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+
+function Header() {
+  return (
+    <>
+      <AppBar
+        position="static"
+        color="default"
+        sx={{ bgcolor: "background.default", color: "text.primary" }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            sx={{ mr: 2, display: "flex", alignItems: "center" }}
+          >
+            <LocalLibraryIcon />
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ ml: 1.5, flexGrow: 1 }}
+            >
+              BookTracker
+            </Typography>
+          </IconButton>
+
+          <div style={{ flexGrow: 1 }} />
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
+
+export default Header;
