63455bf101e5b2ac9be8eb34f46e68858cc1a9d2
honopio <honorine.piolat@gmail.com>
2025-05-28 12:40:20 +0200

    fix: better grid layout

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -10,116 +10,131 @@ const Dashboard = () => {
       }}
     >
       <Container maxWidth="xl">
-        <Typography variant="h3">My Reading Dashboard</Typography>
+        <Typography variant="h3" mb={4}>
+          My Reading Dashboard
+        </Typography>
 
         <Grid
           container
-          spacing={3}
-          sx={{ height: "calc(100vh - 200px)", margin: "50px" }}
+          spacing={2}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
         >
-          {/* Left column - 2 horizontal boxes */}
-          <Grid size={{ xs: 12, lg: 8 }}>
-            <Grid container spacing={3} sx={{ height: "100%" }}>
-              {/* Currently Reading Box */}
-              <Grid size={12} sx={{ height: "50%" }}>
-                <Paper
-                  elevation={1}
-                  sx={{
-                    height: "100%",
-                    p: 3,
-                    backgroundColor: "#d8e5cd",
-                    border: "1px solid var(--accent)",
-                    borderRadius: 4,
-                    display: "flex",
-                    flexDirection: "column",
-                    boxShadow: "none",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      mb: 2,
-                    }}
-                  >
-                    <Typography
-                      variant="h5"
-                      component="h3"
-                      sx={{
-                        color: "#110e03",
-                        fontWeight: 600,
-                      }}
-                    >
-                      Currently Reading
-                    </Typography>
-                    <Chip
-                      label={`3 books`}
-                      sx={{
-                        backgroundColor: "#6f9c68",
-                        color: "#fdf9f2",
-                        fontSize: "0.875rem",
-                      }}
-                    />
-                  </Box>
-                </Paper>
-              </Grid>
-
-              <Grid size={12} sx={{ height: "50%" }}>
-                <Paper
-                  elevation={1}
-                  sx={{
-                    height: "100%",
-                    p: 3,
-                    backgroundColor: "#aa84cc",
-                    borderRadius: 4,
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      mb: 2,
-                    }}
-                  >
-                    <Typography
-                      variant="h5"
-                      component="h3"
-                      sx={{
-                        color: "#fdf9f2",
-                        fontWeight: 600,
-                      }}
-                    >
-                      Want to Read
-                    </Typography>
-                    <Chip
-                      label={`2 books`}
-                      sx={{
-                        backgroundColor: "#6f4c93",
-                        color: "#fdf9f2",
-                        fontSize: "0.875rem",
-                      }}
-                    />
-                  </Box>
-                </Paper>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          {/* Right column - 1 vertical box */}
-          <Grid size={12} lg={4}>
+          {/* Left column: two stacked areas */}
+          <Grid
+            sx={{
+              flex: 2,
+              minWidth: 0,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            }}
+          >
             <Paper
               elevation={1}
               sx={{
-                height: "100%",
+                height: 220,
+                p: 3,
+                backgroundColor: "#d8e5cd",
+                border: "1px solid var(--accent)",
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "none",
+                mb: 2,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  mb: 2,
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  component="h3"
+                  sx={{
+                    color: "#110e03",
+                    fontWeight: 600,
+                  }}
+                >
+                  Currently Reading
+                </Typography>
+                <Chip
+                  label="nb of books"
+                  sx={{
+                    backgroundColor: "#6f9c68",
+                    color: "#fdf9f2",
+                    fontSize: "0.875rem",
+                  }}
+                />
+              </Box>
+            </Paper>
+            <Paper
+              elevation={1}
+              sx={{
+                height: 220,
+                p: 3,
+                backgroundColor: "#aa84cc",
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "none",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  mb: 2,
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  component="h3"
+                  sx={{
+                    color: "#fdf9f2",
+                    fontWeight: 600,
+                  }}
+                >
+                  Want to Read
+                </Typography>
+                <Chip
+                  label="nb of books"
+                  sx={{
+                    backgroundColor: "#6f4c93",
+                    color: "#fdf9f2",
+                    fontSize: "0.875rem",
+                  }}
+                />
+              </Box>
+            </Paper>
+          </Grid>
+
+          {/* Right column: finished books */}
+          <Grid
+            sx={{
+              flex: 1,
+              minWidth: 0,
+              display: "flex",
+            }}
+          >
+            <Paper
+              elevation={1}
+              sx={{
+                width: "100%",
+                height: 463, // match the combined height of left column
                 p: 3,
                 backgroundColor: "#6f9c68",
                 borderRadius: 4,
                 display: "flex",
                 flexDirection: "column",
+                boxShadow: "none",
               }}
             >
               <Box
@@ -141,7 +156,7 @@ const Dashboard = () => {
                   Finished Books
                 </Typography>
                 <Chip
-                  label={`2 books`}
+                  label="nb of books"
                   sx={{
                     backgroundColor: "#5a7d54",
                     color: "#fdf9f2",
