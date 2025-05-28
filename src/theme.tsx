d501fca7016b095b373bb42aa2f06501eb060221
honopio <honorine.piolat@gmail.com>
2025-05-28 13:58:14 +0200

    refactor: update theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,10 +2,11 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    primary: { main: "#7d70a5" },
+    primary: { main: "#fdf9f2" },
+    success: { main: "#7d70a5" },
     secondary: { main: "#d8e5cd" },
     info: { main: "#6f9c68" },
-    background: { default: "#fdf9f2" },
+    background: { default: "#fdf9f2", paper: "#fdf9f2" },
     text: { primary: "#110e03" },
   },
   typography: {
