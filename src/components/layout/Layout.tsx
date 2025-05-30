bfa2269672f56e9271ff1795a13549ce304c1317
honopio <honorine.piolat@gmail.com>
2025-05-30 16:27:47 +0200

    feat: handle sidebar in layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
     <>
-      <Header />
+      <Header onDrawerOpen={() => setDrawerOpen(true)} />
+      <Sidebar open={drawerOpen} onClose={() => setDrawerOpen(false)} />
       <Outlet />
       <Footer />
     </>
