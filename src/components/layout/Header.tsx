aab6da470fb1187b0982e77ec91d3d441d05dd8f
honopio <honorine.piolat@gmail.com>
2025-05-30 13:21:51 +0200

    feat: add link from logo to home page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -19,26 +20,31 @@ function Header() {
         }}
       >
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            sx={{
-              mr: 2,
-              display: "flex",
-              alignItems: "center",
-              color: "text.primary",
-            }}
-          >
-            <LocalLibraryIcon />
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ ml: 1.5, flexGrow: 1 }}
-              color="text.primary"
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <IconButton
+              size="large"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: "flex",
+                alignItems: "center",
+                color: "text.primary",
+                "&:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
             >
-              BookTracker
-            </Typography>
-          </IconButton>
+              <LocalLibraryIcon />
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ ml: 1.5, flexGrow: 1 }}
+                color="text.primary"
+              >
+                BookTracker
+              </Typography>
+            </IconButton>
+          </Link>
 
           <div style={{ flexGrow: 1 }} />
           <Button>Login</Button>
