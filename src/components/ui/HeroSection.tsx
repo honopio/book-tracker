ef2f421d3ee1f1eef984f02fa3a727fa4e0e647c
honopio <honorine.piolat@gmail.com>
2025-06-02 20:44:46 +0200

    feat: integrate image to hero section

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { NavLink } from "react-router-dom";
 import theme from "../../theme";
+import books from "../../assets/books.png";
 
 // Hero section component
 const HeroSection: React.FC = () => (
@@ -15,38 +16,62 @@ const HeroSection: React.FC = () => (
       mb: 4,
       borderRadius: 2,
       textAlign: "center",
+      display: "flex",
     }}
   >
-    <Typography variant="h2" component="h1" gutterBottom>
-      Track Your Reading Journey
-    </Typography>
-    <Typography
-      variant="h6"
-      color="text.secondary"
-      sx={{ mb: 4, maxWidth: "600px", mx: "auto" }}
+    <Box
+    // sx={{
+    //   flex: 1,
+    // }}
     >
-      Organize your books and track your progress with ease. Join our community
-      of readers today!
-    </Typography>
-    <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
-      <Button
-        variant="contained"
-        size="large"
-        component={NavLink}
-        to="/login"
-        sx={{ px: 4 }}
+      <Typography variant="h2" component="h1" gutterBottom>
+        Track Your Reading Journey
+      </Typography>
+      <Typography
+        variant="h6"
+        color="text.secondary"
+        sx={{ mb: 4, maxWidth: "600px", mx: "auto" }}
       >
-        Get Started
-      </Button>
-      <Button
-        variant="outlined"
-        size="large"
-        component={NavLink}
-        to="/dashboard"
-        sx={{ px: 4 }}
-      >
-        Explore Demo
-      </Button>
+        Organize your books and track your progress with ease. Join our
+        community of readers today!
+      </Typography>
+      <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
+        <Button
+          variant="contained"
+          size="large"
+          component={NavLink}
+          to="/login"
+          sx={{ px: 4 }}
+        >
+          Get Started
+        </Button>
+        <Button
+          variant="outlined"
+          size="large"
+          component={NavLink}
+          to="/dashboard"
+          sx={{ px: 4 }}
+        >
+          Explore Demo
+        </Button>
+      </Box>
+    </Box>
+    <Box
+      sx={{
+        flex: 1,
+        display: "flex",
+      }}
+    >
+      <img
+        src={books}
+        alt="Books"
+        style={{
+          width: "100%",
+          maxWidth: "400px",
+          height: "auto",
+          margin: "0 auto",
+        }}
+      />
     </Box>
   </Paper>
 );
