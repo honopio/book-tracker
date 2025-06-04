0b8bfd21b8ac3042fb43fdccf971e8e815822693
honopio <honorine.piolat@gmail.com>
2025-06-04 14:01:00 +0200

    feat: add formReducer file for reusability

---
+++ src/hooks/formReducer.ts
@@ -0,0 +1,29 @@
+export const formReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "set_status":
+      return { ...state, status: action.newValue };
+    case "set_rating":
+      return { ...state, rating: action.newValue };
+    case "set_current":
+      return { ...state, current: action.newValue };
+    case "set_total":
+      return { ...state, total: action.newValue };
+    case "set_comment":
+      return { ...state, comment: action.newValue };
+    case "set_track_progress":
+      return { ...state, trackProgress: action.newValue };
+    case "set_all":
+      return action.newValues;
+    default:
+      return state;
+  }
+};
+
+export const initialFormState = {
+    status: "want-to-read",
+    rating: 0,
+    current: undefined,
+    total: undefined,
+    comment: "",
+    trackProgress: false,
+};
